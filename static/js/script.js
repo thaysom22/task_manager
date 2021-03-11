f3042d95c70480d163f29cd88b64085de2b5e7e0
thaysom22 <thaysom22@gmail.com>
2021-03-11 11:08:59 +0000

    add task collapsible setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,11 @@
 // initialize sidenav
-const options = {
-    edge: "right",
-}
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+    var instances = M.Sidenav.init(elems, {edge: "right"});
+  });
+
+// initialize collapisble accordian element
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
   });
