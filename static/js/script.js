eb0f25a6ff675da6d1afa87563803308e5143a6c
thaysom22 <thaysom22@gmail.com>
2021-03-12 06:51:29 +0000

    add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -10,8 +10,22 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Collapsible.init(elems);
   });
 
-  // initialize .tooltipped
+// initialize .tooltipped
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.tooltipped');
     var instances = M.Tooltip.init(elems);
+  });
+
+// initialize .datepicker
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, {
+        autoClose: true, 
+        format: "dd mmmm yyyy", 
+        yearRange: 3, 
+        minDate: new Date(),
+        i18n: {
+          done: "Select"  
+        }
+    });
   });
